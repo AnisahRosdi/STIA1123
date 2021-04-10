9f7952b11a8d83d055b32e8fd34c5a8a101dd95e
AnisahRosdi <anisahn042@gmail.com>
2021-04-11 04:19:23 +0800

    scanner

--- exersice2/src/exersice2/main.java
+++ exersice2/src/exersice2/main.java
@@ -1,25 +1,29 @@
 package exersice2;
 
+import java.util.Scanner;
+
 public class main {
 
+	
 	public static void main(String[] args) {
 		
 		//create first new object
 		purse man = new purse();
-		man.printpurse("man","Top Grain Calf Leather", "Bonia",150.5);
-		man.calculateprice(150.5,2);
-		man.discount(0.15);
-		man.totalpayment(7);
 		System.out.println();
+		man.printScanner();
+		man.calculateprice();
+		man.discount(0.9);
+		man.totalpayment();
 		
 		//create second new object purse
 		purse woman = new purse();
-		woman.printpurse("woman","Premium Synthetic Leather","Kita Mini",100.5);
-		woman.calculateprice(100.5,3);
-		woman.discount(0.3);
-		woman.totalpayment(4);
-		System.out.println();
 		
+		System.out.println();
+		woman.printScanner();
+		woman.calculateprice();
+		woman.discount(0.9);
+		woman.totalpayment();
+
 	}
 
 }
--- exersice2/src/exersice2/purse.java
+++ exersice2/src/exersice2/purse.java
@@ -1,39 +1,71 @@
 package exersice2;
 
+import java.util.Scanner;
+
 public class purse {
-	String material;
-	String brand;
-	String gender;
-	double price,discount,calculateprice1,calculateprice2,total;
-	int value,delivery;
+	String m,b,g;
+	double p,discount,calculateprice1,calculateprice2,total;
+	int v,d,choose;
 	
+	//for input
+	Scanner input = new Scanner(System.in);
 	
-	//1st method print
-	void printpurse(String g,String m,String b,double p) {
+	//method 1 = input data and output(print)
+	void printScanner() {
+		System.out.println("Enter material, brand,gender:");
+		 m =input.nextLine();
+		 b =input.nextLine();
+		 g =input.nextLine();
+		
 		System.out.println("Purse for " + g);
 		System.out.println("Material of purse:" + m);
 		System.out.println("Brand of purse:" + b);
-		System.out.println("Price for purse is RM" + p);
-	}
+		System.out.println("");
+	}//end method 1
 	
-	//2nd method calculate price
-	void calculateprice(double p,int v) {
+	
+	//2nd method calculate price(calculateprice1)
+	void calculateprice() {
+		System.out.print("Enter price :RM");
+		 p =input.nextDouble();
+		System.out.print("Enter number of item: ");
+		 v =input.nextInt();
+		
 		calculateprice1= p * v;
-		System.out.println("Number of Item: " + v);
-		System.out.println("Total price is RM" + calculateprice1);
-	}
+		System.out.printf("Price is RM" + "%.2f",calculateprice1);
+		System.out.println("");
+	}//end method 2
+	
 	
 	//3rd method calculate discount
 	void discount(double d) {
-		calculateprice2= d * calculateprice1;
-		System.out.println("Discount is " + d + "%");
-		System.out.println("Price after discount is RM" + calculateprice2);
-	}
+		if (calculateprice1 >=150) {
+			
+	    calculateprice2= d * calculateprice1;
+		System.out.println("Discount is 10%");
+		System.out.printf("Price after discount is RM" + "%.2f\n",calculateprice2);
+		}else
+			{
+			calculateprice2 =calculateprice1;
+			System.out.println("No discount");
+			}//end if else
+	}//end method 3
 	
+		
 	//4th method totalpayment
-	void totalpayment(int d) {
-		total= d + calculateprice2;
-		System.out.println("Delivery charge is RM" + d);
-		System.out.println("Total payment : RM" + total);
-	}
+	void totalpayment() {
+		System.out.print("Choose delivery 1(fast) or 2(standard):");
+		 choose=input.nextInt();
+		
+		if(choose == 1) {
+			System.out.println("Delivery charge is RM 14");
+			total= 14 + calculateprice2;
+			System.out.printf("Total payment : RM" + "%.2f\n",total);
+		}else
+		{
+			System.out.println("Delivery Charge is RM7");
+		    total= 7 + calculateprice2;
+		    System.out.printf("Total payment : RM" + "%.2f\n",total);
+		}//end if else
+	}//end method 4
 }
